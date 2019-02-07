adf168b24a80ce71befd1ece214fefc8038ce780
= <=>
2019-02-07 13:44:10 -0500

    added starter Xcode project (no code yet)

---
+++ JBus/JBus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JBus
+//
+//  Created by William Thomas on 2/7/19.
+//  Copyright © 2019 William Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
