16e91284fb7f89f05b1150356c8a33f6655ef1c2
Jizhen Wang <jzwang43@gmail.com>
2019-05-25 23:40:31 -0400

    changed to current camera zoom

--- JBus/JBus/Map/MapViewController.swift
+++ JBus/JBus/Map/MapViewController.swift
@@ -23,9 +23,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate  {
     }
     
     override func loadView() {
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: lastLocation.coordinate.latitude, longitude: lastLocation.coordinate.longitude, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: lastLocation.coordinate.latitude, longitude: lastLocation.coordinate.longitude, zoom: 15.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         
         view = mapView
